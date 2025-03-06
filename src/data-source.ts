4580612c8a9c76e151dfdad11e8d28da2070e605
Pluri45 <snipertft45@gmail.com>
2025-03-06 16:07:16 +0100

    latest chnages

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,26 @@
 import { DataSource } from 'typeorm';
 import { User } from './models/User';
+import fs from "fs";
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USERNAME,
+  username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   synchronize: true,
   logging: true,
   entities: [User],
   subscribers: [],
+  ssl:
+    process.env.DB_USE_SSL === "true"
+      ? {
+          rejectUnauthorized: true,
+          ca: fs
+            .readFileSync(__dirname + "/database/ca-certificate.crt")
+            .toString(),
+        }
+      : false,
   migrations: [],
 }); 
