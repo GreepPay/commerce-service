b4e9a288c46ebcec706ba5a7625faa3bb2771048
Ayomide <ayomide@ceedlab.co>
2025-10-22 18:20:34 +0100

    fix: add categories

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,7 +1,7 @@
-import { Entity, Column, ManyToMany } from "typeorm";
+import { Entity, Column, ManyToMany, OneToMany } from "typeorm";
 import { BaseModel } from "./BaseModel";
 import { Product } from "./Product";
-import type { Product as Producttype} from "./Product";
+import type { Product as Producttype } from "./Product";
 
 @Entity()
 export class Category extends BaseModel {
@@ -11,6 +11,6 @@ export class Category extends BaseModel {
   @Column({ type: "varchar", length: 255, unique: true })
   slug!: string;
 
-  @ManyToMany(() => Product, (product) => product.categories)
+  @OneToMany(() => Product, (product) => product.category)
   products!: Producttype[];
 }
