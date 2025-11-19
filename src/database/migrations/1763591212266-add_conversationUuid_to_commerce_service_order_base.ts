f7718f48dd301c491c34895f7e8c5d8cc479776e
Ayomide <ayomide@ceedlab.co>
2025-11-19 23:33:54 +0100

    orders with conversation

--- src/database/migrations/1763591212266-add_conversationUuid_to_commerce_service_order_base.ts
+++ src/database/migrations/1763591212266-add_conversationUuid_to_commerce_service_order_base.ts
@@ -4,16 +4,16 @@ export class AddConversationUuidToCommerceServiceOrderBase1763591212266
   implements MigrationInterface
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            ALTER TABLE "commerce_service"."order" 
-            ADD COLUMN conversationUuid VARCHAR(255) NULL
-        `);
+    // await queryRunner.query(`
+    //         ALTER TABLE "commerce_service"."order"
+    //         ADD COLUMN conversationUuid VARCHAR(255) NULL
+    //     `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`
-            ALTER TABLE "commerce_service"."order" 
-            DROP COLUMN conversationUuid
-        `);
+    // await queryRunner.query(`
+    //         ALTER TABLE "commerce_service"."order"
+    //         DROP COLUMN conversationUuid
+    //     `);
   }
 }
